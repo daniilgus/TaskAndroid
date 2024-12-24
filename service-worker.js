a39c445689741d412087947a2c731ca639e7e329
Daniil Yusov <122688428+daniilgus@users.noreply.github.com>
2024-12-24 04:13:20 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,19 @@
-const CACHE_NAME = 'task-pwa-cache-v1';
+// service-worker.js
+const CACHE_NAME = 'my-app-cache-v1';
 const urlsToCache = [
-    '/',
-    '/authorization.html',
-    '/Administrator.html',
-    '/registration.html',
-    '/img.jpg',
-    '/style.css', // Если у вас есть файл стилей
-    // Добавьте другие файлы, которые нужно кэшировать
+    'authorization.html',
+    'registration.html',
+    'Administrator.html',
+    'manifest.json',
+    'img/icon.png'
+    // Добавьте другие файлы, которые хотите кэшировать
 ];
 
-// Установка кэша
+// Установка service worker и кэширование файлов
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installing...');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Service Worker: Caching files');
                 return cache.addAll(urlsToCache);
             })
     );
@@ -23,26 +21,23 @@ self.addEventListener('install', (event) => {
 
 // Обработка запросов
 self.addEventListener('fetch', (event) => {
-    console.log('Service Worker: Fetching...', event.request.url);
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
-                // Возвращаем ответ из кэша, если он есть, иначе делаем сетевой запрос
+                // Возвращаем кэшированный файл, если он есть
                 return response || fetch(event.request);
             })
     );
 });
 
-// Обновление кэша
+// Удаление устаревших кэшей
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activating...');
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        console.log('Service Worker: Deleting old cache', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
